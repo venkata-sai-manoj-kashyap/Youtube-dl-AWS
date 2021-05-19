770419f99e14a1d205c56fb0a488b01bbdafb2c8
ramyasreejavvaji <j.ramya56@gmail.com>
2021-05-19 15:12:22 -0400

    Add Streaming service

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, send_file, session
+from flask import Flask, request, render_template, send_file, session, redirect, url_for
 from handl_resources import fetch_formats, download_file_from_link
 
 app = Flask(__name__)
@@ -15,7 +15,7 @@ def my_form_post():
     link = request.form['YoutubeLink']
     session['link'] = link
     available_formats = fetch_formats(link)
-    return render_template('format.html', available_formats=available_formats, link=link)
+    return render_template('format.html', available_formats=available_formats)
 
 
 @app.route('/download_link', methods=['POST'])
@@ -23,9 +23,25 @@ def gen_download_link():
     format_code = request.form['Input_Num']
     link = session['link']
     download_link = download_file_from_link(link, format_code)
-    return send_file(download_link, as_attachment=True)
+    session['download'] = download_link
+    return render_template('download_or_stream.html')
 
+
+@app.route('/download')
+def download_file():
+    if 'download' in session:
+        return send_file(session['download'], as_attachment=True)
+    else:
+        return redirect(url_for('/', _method="GET"))
+
+
+@app.route('/stream')
+def download_file():
+    if 'download' in session:
+        return render_template('stream.html', link=session['download'])
+    else:
+        return redirect(url_for('/', _method="GET"))
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
