db596f35a9c08e3de7a9f7a84dc3c0f33222c1e4
ramyasreejavvaji <j.ramya56@gmail.com>
2021-05-21 23:28:56 -0400

    Added Scipt file to automate tasks

---
+++ script.sh
@@ -0,0 +1,6 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install python3-pip -y
+sudo apt install python -y
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
+
