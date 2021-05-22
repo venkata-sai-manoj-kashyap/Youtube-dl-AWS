ded645a3657e45598df28b38bf9a93566a5153f9
ramyasreejavvaji <j.ramya56@gmail.com>
2021-05-21 23:38:59 -0400

    Made changes to script

--- script.sh
+++ script.sh
@@ -3,4 +3,5 @@ sudo apt install python3-pip -y
 sudo apt install python -y
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
-
+sudo pip3 install flask
+sudo pip3 install python-dateutil
